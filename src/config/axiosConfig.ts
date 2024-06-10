ed72d2634f3701fb7cb6e4d29e8a3cb11d4ab454
Nass <belalialive@gmail.com>
2024-06-11 00:05:12 +0200

    sligth change

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:3000", // Remplacez par l'URL de votre backend
+	baseURL: "http://localhost:3000",
 });
 
 axiosInstance.interceptors.request.use(
